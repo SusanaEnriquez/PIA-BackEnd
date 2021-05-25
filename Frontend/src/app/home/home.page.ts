e670a3c161a2dab7e457004a5f83e816a737a537
Orlandolo <orlando.medinamelendez@gmail.com>
2021-05-24 23:50:24 -0500

    Consumo de Api en la aplicacion

--- Frontend/src/app/home/home.page.ts
+++ Frontend/src/app/home/home.page.ts
@@ -5,6 +5,8 @@ import { AddImagenPage } from './../add-imagen/add-imagen.page';
 import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { ImagenModalPage } from '../imagen-modal/imagen-modal.page';
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -13,8 +15,15 @@ import { ImagenModalPage } from '../imagen-modal/imagen-modal.page';
 })
 export class HomePage {
 
-  constructor(private modalCtrl: ModalController) {}
+  public imagenes: Imagen[];
+  private apiUrl = "http://localhost:1323/images";
 
+  constructor(private modalCtrl: ModalController, private http: HttpClient) {
+    http.get<Imagen[]>(this.apiUrl).subscribe(result => {
+      this.imagenes = result;
+    }, error => console.error(error));
+  }
+  /*
   imagenes = [
     'assets/imagenes/meme1.jpg',
     'assets/imagenes/meme2.jpg',
@@ -25,7 +34,7 @@ export class HomePage {
     'assets/imagenes/meme7.png',
     'assets/imagenes/meme8.jpg'
   ];
-
+  */
   EditarImagen(imagen){
     this.modalCtrl.create({
       component: ImagenModalPage,
@@ -43,4 +52,19 @@ export class HomePage {
 
   EliminarImagen() {}
 
+
+  public getImagen(): Observable<Imagen> {
+    return this.http.get<Imagen>(this.apiUrl);
+  }
+
+  obtenerImagen(){
+    console.log(this.imagenes);
+  }
+}
+
+export interface Imagen {
+  id: number; // Contiene la frase
+  titulo: string;
+  descripcion: string; // Identificador la frase aleatoria
+  url: string; // URL que contiene el ID y permite recuperar la frase
 }
--- Frontend/src/app/imagen-modal/imagen-modal.page.ts
+++ Frontend/src/app/imagen-modal/imagen-modal.page.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { Component, OnInit } from '@angular/core';
 import { ModalController, NavParams } from '@ionic/angular';
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-imagen-modal',
@@ -10,11 +12,14 @@ import { ModalController, NavParams } from '@ionic/angular';
 export class ImagenModalPage implements OnInit {
 
   imagenHome: string;
+  public imagen = {id: 0, titulo: "", descripcion: "", url: ""};
+  public imagenes: Imagen[];
+  private apiUrl = "http://localhost:1323/images/";
 
-  constructor(private modalCtrl: ModalController, private navparams: NavParams)
+  constructor(private modalCtrl: ModalController, private navparams: NavParams, private http: HttpClient)
   {
     this.imagenHome = this.navparams.get('imagen');
-    console.log(this.imagenHome);
+    console.log(this.imagenHome);    
   }
 
   ngOnInit() {}
@@ -25,6 +30,26 @@ export class ImagenModalPage implements OnInit {
 
   Save(){
     this.modalCtrl.dismiss();
+    
+    var titulo = document.getElementById("titulo").nodeValue;
+    var descripcion = document.getElementById("descripcion").nodeValue;
+    /*
+    this.http.post<Imagen>(this.apiUrl + this.imagenHome, {
+      "titulo": titulo,
+      "descripcion": descripcion
+    }).subscribe(this.imagenes => console.log('Image successful'));
+    */
+    /*
+    document.getElementById("titulo").nodeValue = "";
+    document.getElementById("descripcion").nodeValue = "";
+    */
   }
 
+}
+
+export interface Imagen {
+  id: number; // Contiene la frase
+  titulo: string;
+  descripcion: string; // Identificador la frase aleatoria
+  url: string; // URL que contiene el ID y permite recuperar la frase
 }
